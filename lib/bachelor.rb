a17882f3e43353939cbda8bab725474266419238
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-24 20:33:16 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,77 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  #data is the hash of infomation
+  data.each do |season_x, contestant_info |
+    #binding.pry 
+    if season_x == season
+      contestant_info.each do |indiv_contest|
+      #binding.pry
+      if indiv_contest["status"] == "Winner"
+        return indiv_contest["name"].split(" ").first
+      end 
+      end 
+  end 
+end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_x,contestant_info|
+    contestant_info.each do |indiv_contest|
+      #binding.pry
+      if indiv_contest["occupation"] == occupation
+      #binding.pry
+      return indiv_contest["name"]
+    end 
+  end 
+  
+end 
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
+  i = 0 # our hometown counter 
+  
+  data.each do |season_x , contestant_info|
+    contestant_info.each do |indiv_contest|
+      #binding.pry
+      if indiv_contest["hometown"] == hometown
+      i += 1 
+      end 
+      
+    end 
+    
+  end 
+   i
 end
 
 def get_occupation(data, hometown)
-  # code here
+  
+  data.each do |season_x , contestant_info|
+    contestant_info.each do |indiv_contest|
+      #binding.pry
+      if indiv_contest["hometown"] == hometown
+        return indiv_contest["occupation"]
+      end 
+    end 
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+contestant_counter = 0
+total_age = 0
+ data.each do |season_x , contestant_info|
+    if season_x == season
+      #binding.pry
+      contestant_info.each do |indiv_contest|
+        #binding.pry
+    total_age += indiv_contest["age"].to_i
+    contestant_counter +=1
+    end 
+  end 
+
+end 
+#binding.pry
+return (total_age.to_f/contestant_counter).round
 end
