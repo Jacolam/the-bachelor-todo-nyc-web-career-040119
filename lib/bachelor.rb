3579270adbeb601c48b89771473b8842fd3eb70d
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-24 20:11:17 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,49 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+  #data is the hash of infomation
+  data.each do |season_x, contestant_info |
+    #binding.pry 
+    if season_x == season
+      contestant_info.each do |indiv_contest|
+      #binding.pry
+      if indiv_contest["status"] == "Winner"
+        return indiv_contest["name"].split(" ").first
+      end 
+      end 
+  end 
+end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season_x,contestant_info|
+    contestant_info.each do |indiv_contest|
+      #binding.pry
+      if indiv_contest["occupation"] == occupation
+      #binding.pry
+      return indiv_contest["name"]
+    end 
+  end 
+  
+end 
+
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  
+  i = 0 # our hometown counter 
+  
+  data.each do |season_x , contestant_info|
+    contestant_info.each do |indiv_contest|
+      #binding.pry
+      if indiv_contest["hometown"] == hometown
+      i += 1 
+      end 
+      
+    end 
+    
+  end 
+   i
 end
 
 def get_occupation(data, hometown)
